df29ebaeea628c5af38db258e984bfd8d63240f0
Ken Kudo <kkudo224@gmail.com>
2023-04-20 17:01:47 +0000

    Done

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,13 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+        for (int[] line : nums) {
+            for (int num:line) {
+                if (num==target) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
